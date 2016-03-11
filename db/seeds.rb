621d94719cdebfd510f56e3ffd240dc4dc68fa54
Borxburn-One <broxburn001@yahoo.co.uk>
2016-03-11 16:52:58 +0000

    redo git commit in proper folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,15 @@
 require_relative('../models/transaction.rb')
+require( 'pry-byebug' )
 
-# Transaction.delete_all
+Transaction.delete_all
 
+
+# transaction1 = Transaction.create( { 
+#           'tr_date' => "2016-03-16",
+#            'merchant_id' => merchant1.id,
+#            'item_id' =>  item1.id,
+#           'value' => 30.25
+#           } )
+
+binding.pry
+nil
